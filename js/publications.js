01342a6103b61178ad08bb3264d06cf622c346fc
Ryan Whaley <whaleyr@pharmgkb.org>
2017-08-05 15:36:22 -0700

    Website#17 Add terminology paper to publications

--- js/publications.js
+++ js/publications.js
@@ -1,34 +1,39 @@
 var _sortByName = function(o1, o2) {
-	return o1.publicationSet.text.localeCompare(o2.publicationSet.text);
+    return o1.publicationSet.text.localeCompare(o2.publicationSet.text);
 };
 
 var _positionCounter = 1;
 Handlebars.registerHelper('position', function() {
-	return _positionCounter++;
+    return _positionCounter++;
 });
 
 jQuery(function($) {
-	$.getJSON('/wp-content/uploads/2015/12/21270786.json', function(data) {
-		var citationMarkup = Handlebars.templates.citation(data);
-		$('#citeMain').html(citationMarkup);
-	});
+    $.getJSON('/wp-content/uploads/2015/12/21270786.json', function(data) {
+        var citationMarkup = Handlebars.templates.citation(data);
+        $('#citeMain').html(citationMarkup);
+    });
 
-	$.getJSON('/wp-content/uploads/2015/12/24479687.json', function(data) {
-		var citationMarkup = Handlebars.templates.citation(data);
-		$('#citeSecond').html(citationMarkup);
-	});
+    $.getJSON('/wp-content/uploads/2015/12/24479687.json', function(data) {
+        var citationMarkup = Handlebars.templates.citation(data);
+        $('#citeSecond').html(citationMarkup);
+    });
 
-	$.getJSON('https://api.pharmgkb.org/v1/cpic/publications',
-		function(data) {
-			data.forEach(function(o) {
-				o.publicationSet.text = o.publicationSet.text.replace(/CPIC?\s/, '');
-			});
+    $.getJSON('https://api.pharmgkb.org/v1/data/literature/15097680', function(data) {
+        var citationMarkup = Handlebars.templates.citation(data);
+        $('#citeTerms').html(citationMarkup);
+    });
 
-			data.sort(_sortByName);
+    $.getJSON('https://api.pharmgkb.org/v1/cpic/publications',
+        function(data) {
+            data.forEach(function(o) {
+                o.publicationSet.text = o.publicationSet.text.replace(/CPIC?\s/, '');
+            });
 
-			Handlebars.partials = Handlebars.templates;
+            data.sort(_sortByName);
 
-			var guidelinesSection = Handlebars.templates.citationTable(data);
-			$('#guidelinesSection').html(guidelinesSection);
-		});
+            Handlebars.partials = Handlebars.templates;
+
+            var guidelinesSection = Handlebars.templates.citationTable(data);
+            $('#guidelinesSection').html(guidelinesSection);
+        });
 }(jQuery));
