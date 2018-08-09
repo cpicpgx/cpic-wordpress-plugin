1fe7bcd4a6eca297aa5673f99340f360532e5e7f
Ryan Whaley <whaleyr@pharmgkb.org>
2018-08-08 21:08:54 -0700

    Switch guidelines page to use CPIC API

--- js/guidelines.js
+++ js/guidelines.js
@@ -1,13 +1,16 @@
 jQuery(function($) {
+  var _distinctify = function(elem, index, self) {return index === self.indexOf(elem);};
 	var _calculateFields = function(o) {
 		o.name = o.name.replace(/CPIC Dosing Guideline for/, '');
+    o['gene'] = o.gene.map(function(f){return f.hgncid;}).filter(_distinctify);
+    o['drug'] = o.drug.map(function(f){return f.name;}).filter(_distinctify);
 	};
 	var _sortByName = function(o1, o2) {
 		return o1.name.localeCompare(o2.name);
 	};
 	var getData = function() {
 		var deferred = $.Deferred();
-		$.getJSON('https://api.pharmgkb.org/v1/data/guideline?source=CPIC&view=base',
+		$.getJSON('https://data.cpicpgx.org/v1/guideline?select=name,url,gene(hgncid),drug(name)',
 			function(data) {
 				data.forEach(_calculateFields);
 				data.sort(_sortByName);
