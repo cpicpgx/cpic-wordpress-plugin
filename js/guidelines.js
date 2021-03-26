035950f43106a45ce2d5270320af338561e9f078
Ryan Whaley <whaleyr@pharmgkb.org>
2021-03-26 13:26:15 -0700

    feat: link the gene on the guidelines page

--- js/guidelines.js
+++ js/guidelines.js
@@ -1,22 +1,8 @@
 jQuery(function($) {
-  var _distinctify = function(elem, index, self) {return index === self.indexOf(elem);};
-	var _sortStrings = function(o1, o2) {
-		return o1.localeCompare(o2);
-	};
-	var _calculateFields = function(o) {
-		o.name = o.name.replace(/CPIC Dosing Guideline for/, '');
-    o['gene'] = o.gene.map(function(f){return f.symbol;}).filter(_distinctify).sort(_sortStrings);
-    o['drug'] = o.guideline_for_drug.map(function(f){return f.name;}).filter(_distinctify).sort(_sortStrings);
-	};
-	var _sortByName = function(o1, o2) {
-		return o1.name.localeCompare(o2.name);
-	};
 	var getData = function() {
 		var deferred = $.Deferred();
-		$.getJSON('https://api.cpicpgx.org/v1/guideline?select=name,url,gene(symbol),guideline_for_drug(name)&order=name',
+		$.getJSON('https://api.cpicpgx.org/v1/guideline_summary_view?order=guideline_name',
 			function(data) {
-				data.forEach(_calculateFields);
-				data.sort(_sortByName);
 				$('#guidelineList').html(Handlebars.templates.guidelines(data));
 				deferred.resolve();
 			});
